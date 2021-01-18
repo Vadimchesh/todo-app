bb7fd364c81447709d8a251c3b49674ca4ac6663
Vadzim Lindo <v.lindo@godeltech.com>
2021-01-18 10:36:04 +0300

    P1-3: -init React app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import TodoList from './todo/TodoList'
+import TodoControls from './todo/TodoControls';
+import TodoAdd from './todo/TodoAdd';
+
+
+function App() {
+const [todos, setTodos] = React.useState ([
+  {id: 1, complited: false, title: 'New task 1'},
+  {id: 2, complited: false, title: 'New task 2'},
+  {id: 3, complited: false, title: 'New task 3'},
+]
+)
+
+function toggleTodo (id) {
+  setTodos (todos.map (todo => {
+    if (todo.id === id) {
+   todo.complited = !todo.complited
+    }
+    return todo
+  })) 
+}
+
+  return (
+    <div className="wrapper">
+      <h1>todos</h1>
+      <div className="content">
+        <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
+        <TodoControls />
+      </div>
+      <div class="bottom__card1"></div>
+      <div class="bottom__card2"></div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/todo/TodoList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+const styles = {
+    container: {
+        cursor: 'pointer',
+        alignItems: 'center',
+    }
+}
+function TodoList(props) {
+    return (
+        <ul style={styles.container}>    
+            {props.todos.map((todo, index) => {
+                return <TodoItem todo={todo} index={index} key={todo.id} onChange={props.onToggle}></TodoItem>
+            }) }
+        </ul>
+    )
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object), 
+    onToggle: PropTypes.func.isRequired
+}
+
+export default TodoList
